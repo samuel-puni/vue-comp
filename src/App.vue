73412d7b51d117752de8e11111ffbf0ac5163aa6
samuel-puni <samuel.puni@gmail.com>
2019-05-27 10:09:34 -0400

    Se dividio en Cabecera, Cuerpo y Pie

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li class="list-group-item" v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
-    </div>
+  <div class="container">
+
+    <header-server-status></header-server-status>
+
+    <hr>
+
+    <list-server-status></list-server-status>
+
+    <hr>
+
+    <footer-server-status></footer-server-status>
+
+  </div>
 </template>
 
 <script>
---
+++ src/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-xs-12">
+        <header>
+          <h1>Server Status</h1>
+        </header>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
